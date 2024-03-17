75faca86dc370f64c3ca5d9b11a0a1141611ed22
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-03-17 18:14:45 +0430

    first components & props

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%);
+  width: 40%;
+  padding: 2px 20px 20px 20px;
+  margin: 50px auto;
+  border-radius: 10px;
+}
+</style>
