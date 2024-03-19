d52b287e8f0f1494794d2cc00596d418e798dcf3
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-03-19 16:55:34 +0430

    adding & styling tabs

--- src/components/learning-resources/TheResorces.vue
+++ src/components/learning-resources/TheResorces.vue
@@ -1,9 +1,11 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('StoredResources')"
+    <base-button
+      @click="setSelectedTab('StoredResources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('AddResource')"
+    <base-button @click="setSelectedTab('AddResource')" :mode="addResButtonMode"
       >Add Resource</base-button
     >
   </base-card>
@@ -21,13 +23,50 @@ export default {
   },
 
   data() {
-    return { selectedTab: 'StoredResources' };
+    return {
+      selectedTab: 'StoredResources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
   },
 
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
     },
+  },
+
+  computed: {
+    storedResButtonMode() {
+      // if (this.selectedTab === 'StoredResources') {
+      //   return 'flat';
+      // } else {
+      //   return null;
+      // }
+      return this.selectedTab === 'StoredResources' ? null : 'flat';
+    },
+
+    addResButtonMode() {
+      return this.selectedTab === 'AddResource' ? null : 'flat';
+    },
+  },
+
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
   },
 };
 </script>
